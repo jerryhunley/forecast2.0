c66faf24047a79fefedf305f9196fced631ccf35
jerryhunley <jerry.hunley@1nhealth.com>
2025-09-12 15:43:56 -0400

    Add files via upload

---
+++ README.md.txt
@@ -0,0 +1,8 @@
+# Recruitment Forecast App
+
+Streamlit app to forecast clinical trial recruitment.
+
+## Installation
+
+```bash
+pip install -r requirements.txt
---
+++ constants.py
@@ -0,0 +1,11 @@
+# constants.py
+
+# --- Constants for Stage Names ---
+STAGE_PASSED_ONLINE_FORM = "Passed Online Form"
+STAGE_PRE_SCREENING_ACTIVITIES = "Pre-Screening Activities"
+STAGE_SENT_TO_SITE = "Sent To Site"
+STAGE_APPOINTMENT_SCHEDULED = "Appointment Scheduled"
+STAGE_SIGNED_ICF = "Signed ICF"
+STAGE_SCREEN_FAILED = "Screen Failed"
+STAGE_ENROLLED = "Enrolled"
+STAGE_LOST = "Lost"
