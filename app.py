6d89758feea7bb0fe6ecc5f51cb74dcca052dad9
jerryhunley <jerry.hunley@1nhealth.com>
2025-09-17 16:04:33 -0400

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-# app.py
-import streamlit as st
-from streamlit.web import cli as stcli
-import sys
-
-if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "pages/1_Home.py"]
-    sys.exit(stcli.main())
